fce52e08e6fd569e1506fd205eccbe59f73c8323
Mostaf fayez <mostafa.fayez.mostafa.fayez@gmail.com>
2026-01-15 17:47:49 +0200

    Add project files.

---
+++ LeedCode-ProbemSolving/Problem-1/Max Consecutive Ones.cs
@@ -0,0 +1,30 @@
+﻿namespace LeetCode_ProblemSolving.Problem_1
+{
+    public class Max_Consecutive_Ones
+    {
+        public int FindMaxConsecutiveOnes(int[] nums)
+        {
+
+            int count = 0;
+            int container = 0;
+
+            for(int i =0; i < nums.Length; i++)
+            {
+                if (nums[i] == 1)
+                    count++;
+                else
+                {
+                    if (container < count)
+                        container = count;
+
+                    count = 0;
+                }
+            }
+
+            if (count > container)
+                return count;
+            else
+                return container;
+        }
+    }
+}
---
+++ LeedCode-ProbemSolving/Program.cs
@@ -0,0 +1,18 @@
+﻿using LeetCode_ProblemSolving.Problem_1;
+
+namespace LeetCode_ProblemSolving
+{
+    internal class Program
+    {
+        #region 1.Max Consecutive Ones
+        //static void Main(string[] args)
+        //{
+        //    int[] nums = [0, 1, 1, 1, 0, 1, 1];
+        //    Max_Consecutive_Ones maxConsecutive = new();
+
+        //    Console.WriteLine($"The Max consecutive one is : {maxConsecutive.FindMaxConsecutiveOnes(nums)}"); 
+        #endregion
+
+    }
+    }
+}
