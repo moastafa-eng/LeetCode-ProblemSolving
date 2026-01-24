f65ce7c3768c82c57d493cc8645243a1a70066b8
Mostaf fayez <mostafa.fayez.mostafa.fayez@gmail.com>
2026-01-24 20:11:29 +0200

    12. Single Number

---
+++ LeedCode-ProbemSolving/easy problems/Problem-12/SingleNumber.cs
@@ -0,0 +1,17 @@
+﻿namespace LeetCode_ProblemSolving.easy_problems.Problem_12
+{
+    public class SingleNumber
+    {
+        public int SingleNumberr(int[] nums)
+        {
+            int output = 0;
+
+            foreach(int n in nums)
+            {
+                output = n ^ output;
+            }
+
+            return output;
+        }
+    }
+}
